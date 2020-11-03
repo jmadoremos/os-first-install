408fe349c7d8e40920bc05534315a6b8e93128e1
jmadoremos <adoremos162@gmail.com>
2020-11-03 13:04:55 +0000

    Updated wording

--- linux/shared/code-amd64/install.sh
+++ linux/shared/code-amd64/install.sh
@@ -15,6 +15,6 @@ curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 echo "[] Adding Visual Studio Code to APT repository..."
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 
-echo -n "[] Install visual studio code..."
+echo -n "[] Installing visual studio code..."
 sudo apt install -y code
 echo " done"
