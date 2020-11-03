d2a8e06d2a5c8271222d6c66db8986510b287718
jmadoremos <adoremos162@gmail.com>
2020-11-03 12:39:09 +0000

    Added instructions for code-amd64

---
+++ linux/shared/code-amd64/README.md
@@ -0,0 +1,9 @@
+# Visual Studio Code | Linux (amd64)
+
+The official Visual Studio Code for linux.
+
+## Installation
+
+```bash
+curl -L https://raw.githubusercontent.com/jmadoremos/os-first-install/master/linux/shared/code-amd64/install.sh | bash
+```
---
+++ linux/shared/code-amd64/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -n "[] Running apt update..."
+sudo apt clean &> /dev/null
+sudo apt update --fix-missing -y &> /dev/null
+echo " done"
+
+echo -n "[] Installing dependencies..."
+sudo apt install -y curl gpg software-properties-common apt-transport-https &> /dev/null
+echo " done"
+
+echo "[] Registering Microsoft GPG keys..."
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+echo "[] Adding Visual Studio Code to APT repository..."
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+
+echo -n "[] Install visual studio code..."
+sudo apt install -y code
+echo " done"
