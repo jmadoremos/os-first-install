6a3850da307761fab2829672061ccf827da47b1e
jmadoremos <adoremos162@gmail.com>
2020-11-08 16:17:09 +0000

    Added docker.io install as pre-requisite

--- linux/shared/pi-hole/install.sh
+++ linux/shared/pi-hole/install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Install docker
+echo -n "[] Running apt update..."
+sudo apt clean &> /dev/null
+sudo apt update --fix-missing -y &> /dev/null
+echo " done"
+
+echo -n "[] Installing docker as pre-requisite..."
+sudo apt install -y docker.io &> /dev/null
+echo " done"
+
 # Create ~/pi-hole directory if not exists
 echo "[] Creating ~/pi-hole directory, if not exists..."
 mkdir ~/pi-hole &> /dev/null
