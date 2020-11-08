d6c004583c2a4889b13a971b7adf4698d05ec84f
jmadoremos <adoremos162@gmail.com>
2020-11-08 16:17:59 +0000

    Removed &> /dev/null

--- linux/shared/pi-hole/install.sh
+++ linux/shared/pi-hole/install.sh
@@ -16,8 +16,8 @@ mkdir ~/pi-hole &> /dev/null
 
 # Download latest docker-compose.yml
 echo "[] Downloading latest docker-compose.yml..."
-curl -L https://raw.githubusercontent.com/jmadoremos/os-first-install/master/linux/shared/pi-hole/res/docker-compose.yml -o ~/pi-hole/docker-compose.yml &> /dev/null
+curl -L https://raw.githubusercontent.com/jmadoremos/os-first-install/master/linux/shared/pi-hole/res/docker-compose.yml -o ~/pi-hole/docker-compose.yml
 
 # Download latest start.sh
 echo "[] Downloading latest start.sh..."
-curl -L https://raw.githubusercontent.com/jmadoremos/os-first-install/master/linux/shared/pi-hole/res/start.sh -o ~/pi-hole/start.sh &> /dev/null
+curl -L https://raw.githubusercontent.com/jmadoremos/os-first-install/master/linux/shared/pi-hole/res/start.sh -o ~/pi-hole/start.sh
