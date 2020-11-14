f0ffa699087398559ea80769d5b768917301f40a
jmadoremos <adoremos162@gmail.com>
2020-11-14 19:40:45 +0000

    Fixed errors

--- linux/shared/openhab/res/docker-run.sh
+++ linux/shared/openhab/res/docker-run.sh
@@ -9,19 +9,20 @@ echo "[] Running in directory: $INSTALL_DIR"
 
 # Stop and remove existing container
 echo "[] Removing \"$CONTAINER_NAME\" container if exists..."
-if [ ! "$(sudo docker ps -q -f name=$CONTAINER_NAME)" ]; then
+if [ "$(sudo docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    sudo docker stop $CONTAINER_NAME &> /dev/null
     if [ "$(sudo docker ps -aq -f status=exited -f name=$CONTAINER_NAME)" ]; then
-        sudo docker rm $CONTAINER_NAME
+        sudo docker rm $CONTAINER_NAME &> /dev/null
     fi
 fi
 
 # Run image as container
-docker run -d \
+echo "[] Starting \"$CONTAINER_NAME\" container..."
+sudo docker run -d \
     --name $CONTAINER_NAME \
     -e USER_ID=$(id -u $OPENHAB_USER) \
     -e GROUP_ID=$(id -g $OPENHAB_USER) \
-    -v "${INSTALL_DIR}/etc/localtime:/etc/localtime:ro" \
-    -v "${INSTALL_DIR}/etc/timezone:/etc/timezone:ro" \
+    -v "/etc/localtime:/etc/localtime:ro" \
     -v "${INSTALL_DIR}/conf:/openhab/conf" \
     -v "${INSTALL_DIR}/userdata:/openhab/userdata" \
     -v "${INSTALL_DIR}/addons:/openhab/addons" \
@@ -29,18 +30,19 @@ docker run -d \
     --restart=unless-stopped \
     openhab/openhab:latest
 
-printf "Starting $CONTAINER_NAME container "
+echo -n "[] Checking status"
 for i in $(seq 1 $TIMEOUT); do
-    if [ "$(sudo docker inspect -f "{{.State.Health.Status}}" $CONTAINER_NAME)" == "healthy" ] ; then
-        printf ' OK'
+    if [ "$(sudo docker inspect -f "{{.State.Status}}" $CONTAINER_NAME)" == "running" ] ; then
+        echo " running."
         exit 0
     else
         sleep 3
-        printf '.'
+        echo -n "."
     fi
 
     if [ $i -eq $TIMEOUT ] ; then
-        echo -e "\nTimed out waiting for container to start, consult the container logs for more info (\`sudo docker logs $CONTAINER_NAME\`)"
+        echo "** timed out **"
+        echo -e "Consult the container logs for more info (\`sudo docker logs $CONTAINER_NAME\`)"
         exit 1
     fi
 done;
